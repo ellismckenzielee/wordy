ebc1471448a7442bed51328b698d7faa3e28d3a6
Ellis Lee <ellismckenzielee@gmail.com>
2022-01-19 16:11:12 +0000

    add API query functionality

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,26 @@
 import styles from "../styles/form.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 const Form = () => {
   const [word, setWord] = useState("");
+  const [result, setResult] = useState({});
+  console.log(result);
   return (
     <div className={styles.form}>
-      <form className={styles.inputForm}>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          axios
+            .get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+            .then(({ data }) => {
+              setResult(data);
+            })
+            .catch((err) => {
+              console.log("Error");
+            });
+        }}
+        className={styles.inputForm}
+      >
         <label for="word">Please enter a word:</label>
         <input
           onChange={(e) => {
@@ -15,6 +31,7 @@ const Form = () => {
           type="text"
           value={word}
         ></input>
+        <button className={styles.submitButton}>Search</button>
       </form>
     </div>
   );
