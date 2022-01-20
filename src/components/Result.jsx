d443898bd8db369d6cabe162dd38234647cf5efc
Ellis Lee <ellismckenzielee@gmail.com>
2022-01-20 17:26:27 +0000

    add DefinitionCard to Result component

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,10 +1,16 @@
 import styles from "../styles/result.module.css";
+import DefinitionCard from "./DefinitionCard";
 const Result = ({ result }) => {
   console.log(result);
+  const wordInfo = result[0];
   if (!Object.keys(result).length) return <></>;
   return (
     <div className={styles.result}>
-      <p>foudn sumt</p>
+      <p>You searched for:</p>
+      <h1>{wordInfo.word}</h1>
+      {wordInfo.meanings[0].definitions.map((definition, indx) => {
+        return <DefinitionCard key={indx} definition={definition} />;
+      })}
     </div>
   );
 };
