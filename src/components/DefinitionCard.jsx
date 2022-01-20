139f70fc50175330316b7689729fe7c8656a47ff
Ellis Lee <ellismckenzielee@gmail.com>
2022-01-20 20:07:11 +0000

    create Definition card

--- src/components/DefinitionCard.jsx
+++ src/components/DefinitionCard.jsx
@@ -3,7 +3,7 @@ const DefinitionCard = ({ definition }) => {
   console.log("def", definition);
   return (
     <div className={styles.definitionCard}>
-      <h3 className={styles.definition}>{definition.definition}</h3>
+      <h4 className={styles.definition}>{definition.definition}</h4>
       <p>{definition.example}</p>
     </div>
   );
