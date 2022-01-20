5fa512db998ef9f6bb4cbf4472e29a7145e5985f
Ellis Lee <ellismckenzielee@gmail.com>
2022-01-20 16:15:41 +0000

    add Main component to App.jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import Form from "./components/Form";
+import Main from "./components/Main";
 import Header from "./components/Header";
 function App() {
   return (
     <div className="App">
       <Header />
-      <Form />
+      <Main />
     </div>
   );
 }
