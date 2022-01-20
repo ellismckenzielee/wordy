b3a82c5cfd23c45e15332bb21bf54d47fd539c33
Ellis Lee <ellismckenzielee@gmail.com>
2022-01-20 20:50:08 +0000

    add err state variable to main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,15 @@
 import Result from "./Result";
 import Form from "./Form";
 import { useState } from "react";
+import styles from "../styles/main.module.css";
 const Main = () => {
   const [result, setResult] = useState({});
+  const [err, setErr] = useState("");
+  console.dir(result);
   return (
-    <div>
-      <Form setResult={setResult} />
-      <Result result={result} />
+    <div className={styles.main}>
+      <Form setResult={setResult} setErr={setErr} />
+      <Result result={result} err={err} />
     </div>
   );
 };
